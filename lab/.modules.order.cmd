73f3b87a11a22828d8e5fd5a62b302ee15135866
s2xxx <s2xxx@rzn-dev1.qtech.ru>
2024-11-20 16:49:21 +0300

    lab2

--- lab/.modules.order.cmd
+++ lab/.modules.order.cmd
@@ -1,1 +1,1 @@
-cmd_/home/.s2xxx/.git/edu/lab/modules.order := {   echo /home/.s2xxx/.git/edu/lab/lab1.ko; :; } > /home/.s2xxx/.git/edu/lab/modules.order
+cmd_/home/.s2xxx/.git/edu/lab/modules.order := {   echo /home/.s2xxx/.git/edu/lab/lab2.ko; :; } > /home/.s2xxx/.git/edu/lab/modules.order
---
+++ lab/lab2.c
@@ -0,0 +1,55 @@
+#include <linux/module.h>
+#include <linux/slab.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("s2xxx");
+
+#define MOD_NAME "Module Lab2 "
+
+#define MIN_VAL	0
+#define MAX_VAL	100
+
+static int m_size = 2;
+
+module_param(m_size, int, 0644);
+MODULE_PARM_DESC(m_count, "Massiv size (1..100)");
+
+int init_module(void)
+{
+	int rv = -EPERM, i = 0, cnt = 0, sum = 0;
+	char *m = NULL;
+	if ((MIN_VAL < m_size) && (MAX_VAL > m_size))
+	{
+		cnt = m_size * m_size;
+		if (NULL != (m = kmalloc(cnt, GFP_KERNEL)))
+		{
+			printk(KERN_INFO MOD_NAME ": size %i\n", m_size);
+			for (i = 0; i < cnt; i++)
+			{
+				m[i] = get_random_u8();
+
+				/* Print now for prevent also one for-loop */
+				if (0 == (i % cnt))
+				{
+					printk(KERN_INFO MOD_NAME "\n");
+				}
+				printk(KERN_INFO MOD_NAME "%i", m[i]);
+
+				sum += m[i];
+			}
+
+			printk(KERN_INFO MOD_NAME "\n summ is %i", sum);
+
+			kfree(m);
+			rv = 0;
+		}
+	}
+
+	return rv;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO MOD_NAME ": exit\n");
+	return;
+}
