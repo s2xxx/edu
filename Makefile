3ad5ce4ea34404c1a9a2bd556d130a7939269ff9
s2xxx <s2xxx@rzn-dev1.qtech.ru>
2024-11-20 11:44:08 +0300

    Start

---
+++ Makefile
@@ -0,0 +1,11 @@
+CURRENT = $(shell uname -r)
+KERNEL_DIR = /lib/modules/$(CURRENT)/build
+PWD = $(shell pwd)
+
+obj-m := hello.o
+
+default:
+	$(MAKE) -C $(KERNEL_DIR) M=$(PWD) modules
+	
+clean:
+	$(MAKE) -C $(KERNEL_DIR) M=$(PWD) clean
--- hello.c
+++ hello.c
@@ -1,6 +1,21 @@
 #include <linux/module.h>
+#include <linux/kernel.h>
 
 MODULE_LICENCE("GPL");
 MODULE_AUTHOR("s2xxx");
 
 
+static int __init hello_init(void)
+{
+	printk(KERN_ALERT "In Hello-module\n");
+	return 0;
+}
+
+static void __init hello_exit(void)
+{
+	printk(KERN_ALERT "Out from hello-module\n");
+	return;
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
